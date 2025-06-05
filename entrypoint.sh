6f91368043753ce9599e7d3527a71bc15cf64a14
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-06-05 12:02:30 +0100

    Ollama pulling models

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+die() {
+    echo
+    echo "$@"
+    exit 1
+}
+
+[ -z "$MODELS_TOLOAD" ] || die "MODELS_TO_LOAD not defined"
+
+# Start Ollama in the background.
+/bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "Downloading Models..."
+for MODEL in $MODELS_TO_LOAD; do
+  echo "🔴 Retrieve $MODEL model..."
+  ollama pull "$MODEL"
+done
+echo "🟢 Done!"
+
+# Wait for Ollama process to finish.
+wait $pid
